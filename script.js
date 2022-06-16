421e78f3faa93ead318575157152a4b8a6299f9d
Nemuru-san <nimemanta@gmail.com>
2022-06-17 03:39:44 +0700

    akhir nya selesai

---
+++ script.js
@@ -0,0 +1,45 @@
+function reload() {
+    location.reload();
+}
+
+function fizBuzz() {
+    var fizzbuzzInput = document.getElementById("inputFizzBuzz").value;
+    var number = parseInt(fizzbuzzInput);
+    var output = document.getElementById("resultFizzBuzz").getElementsByTagName("tbody")[0];
+
+    // for (let i = 1; i <= number; i++) {
+
+    //     if (i % 3 == 0 && i % 5 == 0) {
+    //         document.getElementById("resultFizzBuzz").innerHTML = "FIZZBUZZ";
+    //     } else if (i % 3 == 0) {
+    //         document.getElementById("resultFizzBuzz").innerHTML = "FIZZ";
+    //     } else if (i % 5 == 0) {
+    //         document.getElementById("resultFizzBuzz").innerHTML = "BUZZ";
+    //     } else {
+    //         document.getElementById("resultFizzBuzz").innerHTML = number;
+    //     }
+    // }
+
+    for (let i = 1; i <= number; i++) {
+
+
+        if (i % 3 === 0 && i % 5 === 0) {
+
+            output.insertRow().insertCell().innerHTML = 'FizzBuzz'
+
+        } else if (i % 3 === 0) {
+
+            output.insertRow().insertCell().innerHTML = 'Fizz'
+
+        } else if (i % 5 === 0) {
+
+            output.insertRow().insertCell().innerHTML = 'Buzz'
+
+        } else {
+
+            output.insertRow().insertCell().innerHTML = i
+        }
+
+    }
+
+}
